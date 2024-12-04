b863d48bb1a477389c628a0b59f84fe7f29f0f1c
Kent <kent.yuan@Gmail.com>
2024-12-04 10:50:08 +0100

    forgot to commit....

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,21 +1,78 @@
+import Direction.*
+import kotlin.math.abs
+
 // https://adventofcode.com/2023/day/18
 fun main() {
     val today = "Day18"
+    fun toPlans(input: List<String>) =
+        input.map { it.split("[ ()]+".toRegex()).let { Plan(it[0], it[1].toInt(), it[2]) } }
+
+    fun toMatrix(input: List<String>): MatrixDay18 {
+        val points = mutableMapOf<Pair<Int, Int>, Char>()
+        input.forEachIndexed { y, line -> line.forEachIndexed { x, c -> points[x to y] = c } }
+        return MatrixDay18(input[0].lastIndex, input.lastIndex, NotNullMap(points.toMap()))
+    }
 
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
+    fun Pair<Int, Int>.move(direction: Direction, steps: Int) = when (direction) {
+            Up -> (second - steps..second).map { first to it }
+            Down -> (second..second + steps).map { first to it }
+            Left -> (first - steps..first).map { it to second }
+            Right -> (first..first + steps).map { it to second }
+    }
+
+    fun part1(input: List<String>): Int {
+        val plans = toPlans(input)
+        var maxX = 0
+        var minX = 0
+        var maxY = 0
+        var minY = 0
+        val points = mutableSetOf(0 to 0)
+        plans.fold(0 to 0) { acc, plan ->
+            acc.move(plan.direction, plan.value).let {
+                points.addAll( it)
+                maxX = maxOf(maxX, it.first().first, it.last().first)
+                maxX = minOf(minX, it.first().first, it.last().first)
+                maxY = maxOf(maxY, it.first().second, it.last().second)
+                minY = minOf(minY, it.first().second, it.last().second)
+                when (plan.direction) {
+                    Up, Left -> it.first()
+                    Down, Right-> it.last()
+                }
+            }
+        }
+
+        return (minY..maxY).sumOf { y ->
+            points.filter { it.second == y }
+                .sortedBy { it.first }.let { abs(it.last().first - it.first().first) + 1 }
+        }
+    }
+
+    fun part2(input: List<String>): Int {
         return 0
     }
 
-    fun part2(input: List<String>): Long {
-        return 0
-    }
-
-    chkTestInput(part1(testInput), 0L, Part1)
+    chkTestInput(part1(testInput), 62, Part1)
     println("[Part1]: ${part1(input)}")
 
-    chkTestInput(part2(testInput), 0L, Part2)
+    chkTestInput(part2(testInput), 0, Part2)
     println("[Part2]: ${part2(input)}")
+}
+
+class Plan(d: String, val value: Int, val color: String) {
+    val direction = when (d) {
+        "R" -> Right
+        "L" -> Left
+        "U" -> Up
+        "D" -> Down
+        else -> throw IllegalArgumentException("XXX")
+    }
+}
+
+class MatrixDay18(maxX: Int, maxY: Int, points: Map<Pair<Int, Int>, Char>) : Matrix<Char>(maxX, maxY, points) {
+    fun walk(point: Pair<Int, Int>, direction: Direction) {
+
+    }
 }
--- src/Day19.kt
+++ src/Day19.kt
@@ -5,17 +5,81 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
-        return 0
+    fun parseInput(input: List<String>): Pair<List<NotNullMap<String, Int>>, Checker> {
+        val blankIdx = input.indexOf("")
+        println(blankIdx)
+        val (workflowLines, dataLines) = input.let { it.subList(0, blankIdx) to it.subList(blankIdx + 1, it.size) }
+
+        //workflow
+        val wfMap = mutableMapOf<String, List<String>>()
+        workflowLines.forEach { line ->
+            val (name, rules) = line.split("[{}]".toRegex())
+            wfMap[name] = rules.split(',')
+        }
+        val workflows: NotNullMap<String, List<String>> = NotNullMap(wfMap)
+
+        //data
+        val data = dataLines.map { line ->
+            NotNullMap(
+                buildMap {
+                    line.trim('{', '}').split(',').forEach { exp ->
+                        exp.split('=').also { put(it[0], it[1].toInt()) }
+                    }
+                })
+        }
+        return data to Checker(workflows)
+
+    }
+
+
+    fun part1(input: List<String>): Int {
+        val (data, checker) = parseInput(input)
+        return data.sumOf { dataMap ->
+            checker.check(dataMap, "in") * dataMap.values.sum()
+        }
     }
 
     fun part2(input: List<String>): Long {
         return 0
     }
 
-    chkTestInput(part1(testInput), 0L, Part1)
+    chkTestInput(part1(testInput), 19114, Part1)
     println("[Part1]: ${part1(input)}")
 
     chkTestInput(part2(testInput), 0L, Part2)
     println("[Part2]: ${part2(input)}")
+}
+
+data class Checker(val workflow: NotNullMap<String, List<String>>) {
+
+    fun check(dataMap: NotNullMap<String, Int>, name: String): Int {
+        val rules = workflow[name]
+        rules.forEach { rule ->
+            if (rule == "A") return 1
+            if (rule == "R") return 0
+            if (rule[1] in "<>") {
+                val ret = valueCompare(dataMap, rule)
+                if (ret.length > 1) return check(dataMap, ret)
+                if (ret == "A") return 1
+                if (ret == "R") return 0
+                return@forEach
+            }
+            return check(dataMap, rule)
+        }
+        return 0
+    }
+
+
+    private fun valueCompare(dataMap: NotNullMap<String, Int>, rule: String): String {
+
+        val (n, compV, result) = rule.split("[<>:]".toRegex())
+        val op = rule[1]
+        val chkResult = when (op) {
+            '>' -> dataMap[n] > compV.toInt()
+            '<' -> dataMap[n] < compV.toInt()
+            else -> error("XXX")
+        }
+        return if (chkResult) result else ""
+    }
+
 }
